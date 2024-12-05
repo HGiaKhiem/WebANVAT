6fb303c37e99d8459a822e575f7f62d5a324cb6e
HuyRC <huyluong985@gmail.com>
2024-12-05 14:38:19 +0700

    remake cart

--- WebAnVat/Controllers/CartController.cs
+++ WebAnVat/Controllers/CartController.cs
@@ -13,35 +13,44 @@ namespace WebAnVat.Controllers
     {
         // GET: Cart
         public string conStr = "workstation id=BanDoAnVatVer2.mssql.somee.com;packet size=4096;user id=HuyKhiemLong123_SQLLogin_1;pwd=t5fpvoy7nc;data source=BanDoAnVatVer2.mssql.somee.com;persist security info=False;initial catalog=BanDoAnVatVer2;TrustServerCertificate=True";
-        public ActionResult deleteItem(int ID_GioHang)
-        {
-            string query = "delete from GioHang where ID_GioHang = @ID_GioHang";
-            using (SqlConnection conn = new SqlConnection(conStr))
-            {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@ID_GioHang", ID_GioHang);
-                cmd.ExecuteNonQuery();
-            }    
-            return RedirectToAction("loadCart","Home");
-        }
+        //public ActionResult deleteItem(int ID_GioHang)
+        //{
+        //    string query = "delete from GioHang where ID_GioHang = @ID_GioHang";
+        //    using (SqlConnection conn = new SqlConnection(conStr))
+        //    {
+        //        conn.Open();
+        //        SqlCommand cmd = new SqlCommand(query, conn);
+        //        cmd.Parameters.AddWithValue("@ID_GioHang", ID_GioHang);
+        //        cmd.ExecuteNonQuery();
+        //    }    
+        //    return RedirectToAction("loadCart","Home");
+        //}
 
         
-        public ActionResult editItem(int ID_GioHang, int ID_Mon)
+        public ActionResult deleteItem(int  id_mon)
         {
-            string query = "delete from GioHang where ID_GioHang = @ID_GioHang";
-            using (SqlConnection conn = new SqlConnection(conStr))
+            List<productData> cart = Session["Cart"] as List<productData>;
+            if (cart == null || cart.Count == 0)
             {
-                conn.Open();
-                SqlCommand cmd = new SqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@ID_GioHang", ID_GioHang);
-                cmd.ExecuteNonQuery();
+                TempData["Error"] = "Giỏ hàng hiện đang trống.";
+                return RedirectToAction("loadCart", "Home");
             }
-            return RedirectToAction("detailProducts", "Home", new {id = ID_Mon});
+            for (int i = cart.Count - 1; i >= 0; i--)
+            {
+                if (cart[i].ID_Mon == id_mon)
+                {
+                    cart.RemoveAt(i);
+                    break;
+                }
+            }
+            // Cập nhật lại session
+            Session["Cart"] = cart;
+            return RedirectToAction("loadCart", "Home");
         }
         [HttpPost]
         public ActionResult Pay(DonHang model)
         {
+            // thêm thông tin vào DonHang
             string query = "insert into DonHang(ID_NguoiMua,NgayDatDon,PTThanhToan,DiaChi,GhiChu,TienGiaoHang,DienThoai)values(@ID_NguoiMua,@NgayDatDon,@PTThanhToan,@DiaChi,@GhiChu,@TienGiaoHang,@DienThoai)";
             int idUser = Convert.ToInt32(Session["ID"]);
             decimal tiengiaohang = 150000;
@@ -59,7 +68,7 @@ namespace WebAnVat.Controllers
                 cmd.Parameters.AddWithValue("@DienThoai", model.DienThoai);
                 cmd.ExecuteNonQuery();
             }
-
+            // lấy ra ID_DonHang
             string queryfind_id = "select ID_DonHang from DonHang where ID_NguoiMua=@id_user  and ChiTiet=0";
             int id_donhang = 0;
             using (SqlConnection conn1 = new SqlConnection(conStr))
@@ -73,40 +82,33 @@ namespace WebAnVat.Controllers
                     id_donhang = int.Parse(reader.GetValue(0).ToString());
                 }
             }
+            // lấy ra session lưu trữ các món đã đặt
             List<productData> cart = Session["Cart"] as List<productData>;
             if (cart == null || cart.Count == 0)
             {
                 return View(new List<productData>()); // Trả về giỏ hàng rỗng nếu không có sản phẩm
             }
-            foreach (var item in cart)
+            // thực hiện thêm vào chi tiết đơn hàng 
+            for (int i = cart.Count - 1; i >= 0; i--)
             {
-                decimal giathucte = 0;
-                string querygiamon = "select GiaSauKhiGiam from Mon where ID_Mon=@id_mon";
-                using (SqlConnection conn2=new SqlConnection(conStr))
-                {
-                    conn2.Open();
-                    SqlCommand cmd = new SqlCommand(querygiamon, conn2);
-                    cmd.Parameters.AddWithValue("@id_mon", item.ID_Mon);
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    if (reader.Read())
-                    {
-                        giathucte=decimal.Parse(reader.GetValue(0).ToString());
-                    }
-                }
-                 string queryctdh = "insert into ChiTietDonHang (ID_Don,ID_Mon,SoLuong,GiaThucTe,ChiTietMonAn_JSON) " +
-                        "values (@ID_Don,@ID_Mon,@SoLuong,@GiaThucTe,@ChiTietMonAn_JSON)";
-                using (SqlConnection conn3=new SqlConnection (conStr))
+                 string queryctdh = "insert into ChiTietDonHang (ID_Don,ID_Mon,SoLuong,GiaThucTe,ChiTietMonAn_JSON,ThanhTien) " +
+                        "values (@ID_Don,@ID_Mon,@SoLuong,@GiaThucTe,@ChiTietMonAn_JSON,@ThanhTien)";
+                using (SqlConnection conn3 = new SqlConnection(conStr))
                 {
                     conn3.Open();
                     SqlCommand cmd = new SqlCommand(queryctdh, conn3);
                     cmd.Parameters.AddWithValue("@ID_Don", id_donhang);
-                    cmd.Parameters.AddWithValue("@ID_Mon", item.ID_Mon);
-                    cmd.Parameters.AddWithValue("@SoLuong", item.Quantity);
-                    cmd.Parameters.AddWithValue("@GiaThucTe", item.Price);
-                    string toppingJson = JsonConvert.SerializeObject(item.Topping);
+                    cmd.Parameters.AddWithValue("@ID_Mon", cart[i].ID_Mon);
+                    cmd.Parameters.AddWithValue("@SoLuong", cart[i].Quantity);
+                    cmd.Parameters.AddWithValue("@GiaThucTe", cart[i].Price);
+                    cmd.Parameters.AddWithValue("@ThanhTien", cart[i].Price);
+                    string toppingJson = JsonConvert.SerializeObject(cart[i].Topping);
                     cmd.Parameters.AddWithValue("@ChiTietMonAn_JSON", toppingJson);
                     cmd.ExecuteNonQuery();
                 }
+                // sau khi thêm vào chi tiết đơn hàng sẽ xóa món đã đặt khỏi session
+                cart.Remove(cart[i]);
+                Session["Cart"] = cart;
             }
             string queryupdate = "update  DonHang set ChiTiet=1 where ID_NguoiMua=@iduser and ChiTiet=0";
             using (SqlConnection conn4 = new SqlConnection(conStr))
@@ -116,7 +118,6 @@ namespace WebAnVat.Controllers
                 cmd.Parameters.AddWithValue("@iduser", idUser);
                 cmd.ExecuteNonQuery();
             }
-
             return RedirectToAction("Index", "Home");
         }
     }
--- WebAnVat/Models/productData.cs
+++ WebAnVat/Models/productData.cs
@@ -5,26 +5,26 @@ using System.Web;
 
 namespace WebAnVat.Models
 {
-    public class productData
-    {
-        public int ID_Mon { get; set; }
-        public int ID_LoaiMonAn { get; set; }
-        public string Img { get; set; }
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public string Size { get; set; }
-        public string Sweet { get; set; }
-        public string Tea { get; set; }
-        public string Ice { get; set; }
-        public decimal Price { get; set; }
-        public List<toppingData> Topping { get; set; }
-    }
+        public class productData
+        {
+            public int ID_Mon { get; set; }
+            public int ID_LoaiMonAn { get; set; }
+            public string Img { get; set; }
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+            public string Size { get; set; }
+            public string Sweet { get; set; }
+            public string Tea { get; set; }
+            public string Ice { get; set; }
+            public decimal Price { get; set; }
+            public List<toppingData> Topping { get; set; }
+        }
 
-    public class toppingData
-    {
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-    }
+        public class toppingData
+        {
+            public string Name { get; set; }
+            public int Quantity { get; set; }
+        }
 
 
 }
